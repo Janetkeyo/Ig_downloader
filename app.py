dc1ab316118094a35e6499dec3a2853701137235
Janet Awino <janetkeyo@gmail.com>
2022-03-04 20:39:04 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request, redirect
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
+import shutil
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from ntpath import join
+import os
+
+app = Flask(__name__)
+# defining a route
+@app.route("/", methods=['GET']) # decorator
+def home(): # route handler function
+    # returning a html template
+    return render_template('index.html')
+
+@app.route('/download',methods=['POST', "GET"])
+def download():
+
+    if request.method=='POST':
+        result = request.form
+
+        imglink = result['link']
+        flnm = result['flnm']
+        option = result['options']
+
+        link = imglink
+        filename = flnm
+
+        dir_name = os.path.dirname(os.path.realpath(__file__))
+        c_driver = os.path.join(dir_name, './chromedriver')
+
+        CHROME_DRIVER_PATH = c_driver
+
+        options = webdriver.ChromeOptions()
+        options.add_argument("--headless")
+        driver = webdriver.Chrome(
+            executable_path=CHROME_DRIVER_PATH, options=options)
+
+        By.CLASS_NAME
+        def downloadImage(url, filename):
+            driver.get(url)
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "FFVAD"))
+            )
+            s = element.get_attribute('src')
+            r = requests.get(s, stream=True)
+
+            with open(filename, 'wb') as f:
+                shutil.copyfileobj(r.raw, f)
+
+        def downloadVideo(url, filename):
+            driver.get(url)
+            element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "tWeCl"))
+            )
+            s = element.get_attribute('src')
+            r = requests.get(s, stream=True)
+
+            with open(filename, 'wb') as f:
+                for chunk in r.iter_content(chunk_size=1024*1024):
+                    if chunk:
+                        f.write(chunk)
+        
+        if option == "video":
+            downloadVideo(link, filename+".mp4")
+        elif option == "image":
+            downloadImage(link, filename+".png")
+
+
+        driver.close()
+
+    return redirect("/")
+
+app.run(debug = True) 
